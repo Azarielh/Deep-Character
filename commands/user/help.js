61dd0ef36374cbe02a62442925787f4b2d7c2805
Azarielh <lacazejohann@gmail.com>
2025-08-07 17:03:51 +0200

    fix my git mess2

---
+++ commands/user/help.js
@@ -0,0 +1,44 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const content = require('../../srcs/content/command_content.js');
+const data_service = require('../../srcs/services/data_service.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('help')
+		.setDescription(content.command_content.fr.help.description)
+		.setDescriptionLocalizations({
+			'en-US': content.command_content.en.help.description,
+			'en-GB': content.command_content.en.help.description
+		}),
+
+	async execute(interaction) {
+		await helpCommand(interaction);
+	}
+};
+
+async function helpCommand(interaction) {
+	const lang = await data_service.get_lang(interaction.guild.id);
+	const isAdmin = interaction.member.permissions.has(PermissionFlagsBits.Administrator);
+
+	if (isAdmin) {
+		await sendAdminHelp(interaction, lang);
+	} else {
+		await sendUserHelp(interaction, lang);
+	}
+}
+
+async function sendUserHelp(interaction, lang) {
+	const helpMsg = content.command_content[lang].help.user;
+	await interaction.reply({
+		content: helpMsg,
+		ephemeral: true
+	});
+}
+
+async function sendAdminHelp(interaction, lang) {
+	const helpMsg = content.command_content[lang].help.admin;
+	await interaction.reply({
+		content: helpMsg,
+		ephemeral: true
+	});
+}
